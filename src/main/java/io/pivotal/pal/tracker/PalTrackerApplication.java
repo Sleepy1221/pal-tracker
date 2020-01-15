0e360cfd7fab8ce946bba0b96aa542826519b316
pal_user <michaelmetz@discover.com>
2020-01-15 10:45:28 -0700

    JDBC Template lab

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -1,8 +1,12 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class PalTrackerApplication {
@@ -11,8 +15,14 @@ public class PalTrackerApplication {
     }
 
     @Bean
-    public TimeEntryRepository repo(){
-        return new InMemoryTimeEntryRepository();
+    public TimeEntryRepository repo(@Value("${spring.datasource.url:not found}") String springDatasourceUrl){
+        System.out.println("db: " + springDatasourceUrl);
+        DataSource dataSource = DataSourceBuilder
+                .create()
+                .url(springDatasourceUrl)
+                .build();
+
+        return new JdbcTimeEntryRepository(dataSource);
     }
 
 }
